afc8cf51efb6513c12c9d7f44573ac6c3e188bdd
Aragon013 <luisaragonrios@gmail.com>
2026-07-08 01:53:32 -0700

    chore(git): ignore and untrack electron build artifacts

--- apps/controller-electron/dist-electron/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("electronAPI", {
-    minimizeWindow: () => electron_1.ipcRenderer.invoke("window:minimize"),
-    maximizeWindow: () => electron_1.ipcRenderer.invoke("window:maximize"),
-    closeWindow: () => electron_1.ipcRenderer.invoke("window:close"),
-    runClientSecAudit: (payload) => electron_1.ipcRenderer.invoke("secaudit:client-run", payload),
-});
